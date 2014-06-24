8cb7246c1489b4e6ab89590b2713cfb89aa5f377
Jerick Don San Juan <jerick@icannhas.com>
2014-06-24 09:08:02 +0800

    added TemplateViewMixin

--- wkhtmltopdf/views.py
+++ wkhtmltopdf/views.py
@@ -230,3 +230,77 @@ class PDFTemplateView(TemplateView):
                 context=context,
                 **response_kwargs
             )
+
+
+class PDFTemplateMixin(object):
+    """Class-based view for HTML templates rendered to PDF."""
+
+    # Filename for downloaded PDF. If None, the response is inline.
+    filename = 'rendered_pdf.pdf'
+
+    # Send file as attachement. If True render content in the browser.
+    show_content_in_browser = False
+
+    # Filenames for the content, header, and footer templates.
+    template_name = None
+    header_template = None
+    footer_template = None
+
+    # TemplateResponse classes for PDF and HTML
+    response_class = TemplateResponse
+    pdf_response_class = PDFTemplateResponse
+
+    # Command-line options to pass to wkhtmltopdf
+    cmd_options = {
+        # 'orientation': 'portrait',
+        # 'collate': True,
+        # 'quiet': None,
+    }
+
+    def __init__(self, *args, **kwargs):
+        super(PDFTemplateMixin, self).__init__(*args, **kwargs)
+
+        # Copy self.cmd_options to prevent clobbering the class-level object.
+        self.cmd_options = self.cmd_options.copy()
+
+    def get(self, request, *args, **kwargs):
+        if request.GET.get('as', '') == 'pdf':
+            self.response_class = self.pdf_response_class
+
+        return super(PDFTemplateMixin, self).get(request, *args, **kwargs)
+
+    def get_filename(self):
+        return self.filename
+
+    def get_cmd_options(self):
+        return self.cmd_options
+
+    def render_to_response(self, context, **response_kwargs):
+        """
+        Returns a PDF response with a template rendered with the given context.
+        """
+        filename = response_kwargs.pop('filename', None)
+        cmd_options = response_kwargs.pop('cmd_options', None)
+
+        if issubclass(self.response_class, PDFTemplateResponse):
+            if filename is None:
+                filename = self.get_filename()
+
+            if cmd_options is None:
+                cmd_options = self.get_cmd_options()
+
+            # response_kwargs.setdefault('content_type', self.content_type)
+            return self.response_class(
+                request=self.request,
+                template=self.get_template_names(),
+                context=context,
+                filename=filename,
+                show_content_in_browser=self.show_content_in_browser,
+                header_template=self.header_template,
+                footer_template=self.footer_template,
+                cmd_options=cmd_options,
+            )
+        else:
+            return super(PDFTemplateMixin, self).render_to_response(
+                context=context,
+            )
